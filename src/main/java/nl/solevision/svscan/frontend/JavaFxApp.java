b01c573ae1dfbd059b3b8f8b62460802cc29eb4a
Dirk Hermans <diher@mac.home>
2024-12-28 14:20:55 +0100

    refactor and add new form elements

--- src/main/java/nl/solevision/svscan/frontend/JavaFxApp.java
+++ src/main/java/nl/solevision/svscan/frontend/JavaFxApp.java
@@ -1,83 +1,26 @@
 package nl.solevision.svscan.frontend;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import nl.solevision.svscan.SvScanApplication;
-import nl.solevision.svscan.backend.PersonalDataService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class JavaFxApp extends Application {
 
-    private ApplicationContext springContext;
+
+    PersonalDataScene personalDataScene = new PersonalDataScene();
 
     @Override
     public void init() throws Exception {
-        // Initialize Spring Application Context
-        springContext = new AnnotationConfigApplicationContext(SvScanApplication.class);
-
-        System.out.println("Java version: " + System.getProperty("java.version"));
-        System.out.println("Java home: " + System.getProperty("java.home"));
-
+        ApplicationContext springContext = new AnnotationConfigApplicationContext(SvScanApplication.class);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        // Get a Bean from the Spring Context
-        PersonalDataService myService = springContext.getBean(PersonalDataService.class);
-
-        GridPane gridPane = new GridPane();
-        gridPane.setPadding(new Insets(10, 10, 10, 10));
-        gridPane.setVgap(8);
-        gridPane.setHgap(10);
-
-        Label lblTitle = new Label("Vul aub onderstaande gegevens in.");
-        Label lblFirstName = new Label("Voornaam");
-        Label lblMiddleName = new Label("Tussenvoegsel");
-        Label lblSurName = new Label("Familienaam");
-        Label lblDateOfBirth = new Label("Geboortedatum");
-        Label lblEmail = new Label("E-mail");
-
-        TextField txtFirstName = new TextField();
-        TextField txtMiddleName = new TextField();
-        TextField txtSurName = new TextField();
-        TextField txtDateOfBirth = new TextField();
-        TextField txtEmail = new TextField();
-
-        Button button = new Button();
-        button.setText("Volgende");
-
-        GridPane.setConstraints(lblTitle, 1, 0);
-        GridPane.setConstraints(lblFirstName, 0, 2);
-        GridPane.setConstraints(lblMiddleName, 0, 3);
-        GridPane.setConstraints(lblSurName, 0, 4);
-        GridPane.setConstraints(lblDateOfBirth, 0, 5);
-        GridPane.setConstraints(lblEmail, 0, 6);
-
-        GridPane.setConstraints(txtFirstName, 1, 2);
-        GridPane.setConstraints(txtMiddleName, 1, 3);
-        GridPane.setConstraints(txtSurName, 1, 4);
-        GridPane.setConstraints(txtDateOfBirth, 1, 5);
-        GridPane.setConstraints(txtEmail, 1, 6);
-
-        GridPane.setConstraints(button, 1, 7);
-
-        gridPane.getChildren().addAll(lblTitle,
-                lblFirstName, lblMiddleName, lblSurName, lblDateOfBirth, lblEmail,
-                txtFirstName, txtMiddleName, txtSurName, txtDateOfBirth, txtEmail,
-                button);
-
-        Scene scene = new Scene(gridPane, 400, 300);
 
         stage.setTitle("SoleVision scanner");
-        stage.setScene(scene);
+        stage.setScene(personalDataScene.getPersonalDataForm());
         stage.show();
     }
 }
---
+++ src/main/java/nl/solevision/svscan/frontend/PersonalDataScene.java
@@ -0,0 +1,130 @@
+package nl.solevision.svscan.frontend;
+
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+public class PersonalDataScene {
+    
+    public Scene getPersonalDataForm() {
+        GridPane gridPane = getGridPane();
+
+        gridPane.getChildren().addAll(getLblTitle(), getLblFirstName(), getTxtFirstName(),
+                getLblMiddleName(), getTxtMiddleName(), getLblSurname(), getTxtSurname(),
+                getLblDateOfBirth(), getTxtDateOfBirth(), getLblEmail(), getTxtEmail(),
+                getLblSex(), getRadioMale(), getRadioFemale(), getBtnNext());
+
+        return new Scene(gridPane, 400, 300);
+    }
+
+    private static Button getBtnNext() {
+        Button button = new Button();
+        button.setText("Volgende");
+        GridPane.setConstraints(button, 1, 9);
+        return button;
+    }
+
+    private static RadioButton getRadioFemale() {
+        RadioButton radioFemale = new RadioButton();
+        radioFemale.setText("Vrouwelijk");
+        GridPane.setConstraints(radioFemale, 1, 8);
+        return radioFemale;
+    }
+
+    private static RadioButton getRadioMale() {
+        RadioButton radioMale = new RadioButton();
+        radioMale.setText("Mannelijk");
+        GridPane.setConstraints(radioMale, 1, 7);
+        return radioMale;
+    }
+
+    private static Label getLblSex() {
+        Label lblSex = new Label("Geslacht(*)");
+        GridPane.setConstraints(lblSex, 0, 7);
+        return lblSex;
+    }
+
+    private static TextField getTxtEmail() {
+        TextField txtEmail = new TextField();
+        txtEmail.setPromptText("Vul hier uw e-mailadres in aub");
+        GridPane.setConstraints(txtEmail, 1, 6);
+        return txtEmail;
+    }
+
+    private static Label getLblEmail() {
+        Label lblEmail = new Label("E-mail(*)");
+        GridPane.setConstraints(lblEmail, 0, 6);
+        return lblEmail;
+    }
+
+    private static TextField getTxtDateOfBirth() {
+        TextField txtDateOfBirth = new TextField();
+        txtDateOfBirth.setPromptText("Vul hier uw geboortedatum in aub");
+        GridPane.setConstraints(txtDateOfBirth, 1, 5);
+        return txtDateOfBirth;
+    }
+
+    private static Label getLblDateOfBirth() {
+        Label lblDateOfBirth = new Label("Geboortedatum(*)");
+        GridPane.setConstraints(lblDateOfBirth, 0, 5);
+        return lblDateOfBirth;
+    }
+
+    private static TextField getTxtSurname() {
+        TextField txtSurname = new TextField();
+        txtSurname.setPromptText("Vul hier uw familienaam in aub");
+        GridPane.setConstraints(txtSurname, 1, 4);
+        return txtSurname;
+    }
+
+    private static Label getLblSurname() {
+        Label lblSurname = new Label("Familienaam(*)");
+        GridPane.setConstraints(lblSurname, 0, 4);
+        return lblSurname;
+    }
+
+    private static TextField getTxtMiddleName() {
+        TextField txtMiddleName = new TextField();
+        txtMiddleName.setPromptText("Vul eventueel een tussenvoegsel in");
+        GridPane.setConstraints(txtMiddleName, 1, 3);
+        return txtMiddleName;
+    }
+
+    private static Label getLblMiddleName() {
+        Label lblMiddleName = new Label("Tussenvoegsel");
+        GridPane.setConstraints(lblMiddleName, 0, 3);
+        return lblMiddleName;
+    }
+
+    private static TextField getTxtFirstName() {
+        TextField txtFirstName = new TextField();
+        txtFirstName.setPromptText("Vul hier uw voornaam in aub");
+        GridPane.setConstraints(txtFirstName, 1, 2);
+        return txtFirstName;
+    }
+
+    private static Label getLblFirstName() {
+        Label lblFirstName = new Label("Voornaam(*)");
+        GridPane.setConstraints(lblFirstName, 0, 2);
+        return lblFirstName;
+    }
+
+    private static Label getLblTitle() {
+        Label lblTitle = new Label("Vul aub onderstaande gegevens in.");
+        GridPane.setConstraints(lblTitle, 0, 0);
+        return lblTitle;
+    }
+
+    private static GridPane getGridPane() {
+        GridPane gridPane = new GridPane();
+
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+        gridPane.setVgap(8);
+        gridPane.setHgap(10);
+        return gridPane;
+    }
+}
