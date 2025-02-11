3128aacf7a99c2cfdf2a495edb05995ab4275138
DirkHermans <hermans.dirk@hotmail.com>
2025-02-11 20:41:59 +0100

    make a neat welcome view

--- src/main/java/nl/solevision/svscan/gui/view/WelcomeView.java
+++ src/main/java/nl/solevision/svscan/gui/view/WelcomeView.java
@@ -4,16 +4,36 @@ import nl.solevision.svscan.gui.navigation.NavigationObserver;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 
 public class WelcomeView extends StyledPanel {
 
     public WelcomeView(NavigationObserver observer) {
-        setLayout(new BorderLayout());
 
-        add(new JLabel("Welkom bij SVScan", SwingConstants.CENTER), BorderLayout.CENTER);
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        JButton startButton = new StyledButton("Start");
-        startButton.addActionListener(e -> observer.onNavigate("CUSTOMER_DETAILS"));
-        add(startButton, BorderLayout.SOUTH);
+        ImageIcon imgSoleVision = new ImageIcon(Objects.requireNonNull(getClass().getResource("/logoSoleVision25perc.png")));
+        ImageIcon imgNoShoes = new ImageIcon(Objects.requireNonNull(getClass().getResource("/logoNoShoes12perc.jpg")));
+
+        JLabel lblImgSoleVision = new JLabel(imgSoleVision);
+        JLabel lblNoShoes = new JLabel("Niet met schoenen betreden aub.");
+        JLabel lblImgNoShoes = new JLabel(imgNoShoes);
+        JButton btnStart = new StyledButton("Start");
+        btnStart.addActionListener(e -> observer.onNavigate("CUSTOMER_DETAILS"));
+
+        lblImgSoleVision.setAlignmentX(Component.CENTER_ALIGNMENT); // Centering
+        lblNoShoes.setAlignmentX(Component.CENTER_ALIGNMENT); // Centering
+        lblImgNoShoes.setAlignmentX(Component.CENTER_ALIGNMENT); // Centering
+        btnStart.setAlignmentX(Component.CENTER_ALIGNMENT); // Centering
+
+        add(Box.createVerticalGlue());
+        add(lblImgSoleVision);
+        add(Box.createRigidArea(new Dimension(0, 100)));
+        add(lblImgNoShoes);
+        add(Box.createRigidArea(new Dimension(0, 10)));
+        add(lblNoShoes);
+        add(Box.createRigidArea(new Dimension(0, 100)));
+        add(btnStart);
+        add(Box.createVerticalGlue());
     }
 }
